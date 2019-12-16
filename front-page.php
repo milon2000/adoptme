0a11dfbc4a7443333f592e5be21508df47b616ab
milon200 <milakozlowska2000@gmail.com>
2019-12-16 19:00:23 +0100

    init

---
+++ front-page.php
@@ -0,0 +1,98 @@
+<?php get_header(); ?>
+
+<section class="about__text-area">
+    <?php if( get_theme_mod( 'about_block') != "" ): ?>
+                    <p class=" adoptme__text about__text">
+                    <?php echo get_theme_mod( 'about_block'); ?>
+    </p>
+    <?php endif;?>
+    <button class="adoptme__button about__button">Read More</button>
+</section><!--.quotation-text-area-->
+
+<section class="how-it-works">
+    <div class="how-it-works__wrapper">
+
+        <div class="how-it-works__column">
+            <figure class="how-it-works__img"><img src="<?php echo get_template_directory_uri(); ?>/images/cats1.png" /></figure>
+            <h4 class="how-it-works__title">Match the cat</h4>
+            <p class="how-it-works__description">Ut enim ad minima veniam, quis nostrum exercitationem ullam.</p>
+        </div><!--.how-it-works__column-->
+
+        <div class="how-it-works__column">
+        <figure class="how-it-works__img"><img src="<?php echo get_template_directory_uri(); ?>/images/cats2.png" /></figure>
+            <h4 class="how-it-works__title">Fill in the questionnaire</h4>
+            <p class="how-it-works__description">Ut enim ad minima veniam, quis nostrum exercitationem ullam.</p>
+        </div><!--.how-it-works__column-->
+
+        <div class="how-it-works__column">
+        <figure class="how-it-works__img"><img src="<?php echo get_template_directory_uri(); ?>/images/cats3.png" /></figure>
+            <h4 class="how-it-works__title">Pick up the cat</h4>
+            <p class="how-it-works__description">Ut enim ad minima veniam, quis nostrum exercitationem ullam.</p>
+        </div><!--.how-it-works__column-->
+
+    </div><!--.how-it-works__wrapper-->
+</section><!--how-it-works-->
+
+
+<section class="featured-cat">
+<?php
+$args = array(
+    'post_type' => 'project',
+    'orderby' => 'rand',
+    'posts_per_page'=> 1
+    
+);
+
+
+$my_query = new WP_Query($args);
+
+    while($my_query->have_posts()){
+      
+        $my_query->the_post(); ?>
+
+        <div class="featured-cat__wrapper">
+            <div class="featured-cat__cat-description">
+            <h3 class="featured-cat__title"><?php the_title();?></h3>
+            <p class="adoptme__text featured-cat__description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+            <a href="<?php the_permalink();?>"><button class="adoptme__button featured-cat__button">Adopt me</button></a>
+            
+        
+    </div><!--.featured-cat__cat-description-->
+    <figure class="featured-cat__cat-photo">
+            <img src="<?php the_post_thumbnail_url();?>"/>
+        </figure><!--.featured-cat__cat-photo-->
+        </div><!--.featured-cat__wrapper--><?php
+        
+  } wp_reset_query();?>
+    </section>
+
+
+<section class="cats">
+<div class="cats__wrapper">
+<ul class="cats__single">
+
+<?php
+$args = array(
+    'post_type' => 'project',
+    'post_per_page' => 50,
+    'nopaging' => true, 
+);
+$projects = new WP_Query($args);
+
+while($projects->have_posts()) {
+    $projects->the_post();
+
+?>
+  
+
+        <li class="cats__list"><a href="<?php the_permalink();?>"><img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), $size = 'companies_thumb');?>"><div><h3 class="cats__title"><?php the_title();?></h3></div></a></li>
+        
+    <?php } wp_reset_query();?>
+    </ul><!--.cats__single-->
+
+</div><!--.cats__wrapper-->
+
+</section>
+
+
+<?php get_footer(); ?>
---
+++ functions.php
@@ -0,0 +1,303 @@
+<?php
+/**
+ * Adopt Me! functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Adopt_Me!
+ */
+
+if ( ! function_exists( 'adoptme_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 */
+	function adoptme_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on Adopt Me!, use a find and replace
+		 * to change 'adoptme' to the name of your theme in all the template files.
+		 */
+		load_theme_textdomain( 'adoptme', get_template_directory() . '/languages' );
+
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menus( array(
+			'menu-1' => esc_html__( 'Primary', 'adoptme' ),
+		) );
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		) );
+
+		// Set up the WordPress core custom background feature.
+		add_theme_support( 'custom-background', apply_filters( 'adoptme_custom_background_args', array(
+			'default-color' => 'ffffff',
+			'default-image' => '',
+		) ) );
+
+		// Add theme support for selective refresh for widgets.
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		/**
+		 * Add support for core custom logo.
+		 *
+		 * @link https://codex.wordpress.org/Theme_Logo
+		 */
+		add_theme_support( 'custom-logo', array(
+			'height'      => 250,
+			'width'       => 250,
+			'flex-width'  => true,
+			'flex-height' => true,
+		) );
+
+		//Projects post type
+		register_post_type('project',
+		array(
+			'rewrite' => array('slug' => 'projects'),
+			'labels' => array(
+				'name' => 'Projects',
+				'singular_name' => 'Project',
+				'add_new_item' => 'Add New Project'
+			),
+			'menu-icon' => 'dashicos-clipboard',
+			'public' => true,
+			'has_archive' => true,
+			'supports' => array(
+				'title', 'thumbnail', 'editor', 'comments', 'custom-fields', 'excerpt'
+			)
+		
+		)
+			);
+
+		add_image_size( 'companies_thumb', 1200, 800, true);
+
+
+
+		//Header text -tera
+
+		add_action( 'customize_register', 'adoptme_header_text' );
+		/*
+		 * Register Our Customizer Stuff Here
+		 */
+		function adoptme_header_text( $wp_customize ) {
+			// Create custom panel.
+			$wp_customize->add_panel( 'text_blocks_header', array(
+				'priority'       => 500,
+				'theme_supports' => '',
+				'title'          => __( 'Header Text', 'adoptme' ),
+				'description'    => __( 'Changing header text.', 'adoptme' ),
+			) );
+
+			$wp_customize->add_section( 'header_quotation_text' , array(
+				'title'    => __('Change Quotation','adoptme'),
+				'panel'    => 'text_blocks_header',
+				'priority' => 10
+			) );
+
+				// Add setting
+				$wp_customize->add_setting( 'header_quot_block', array(
+					'default'           => __( 'default text', 'adoptme' ),
+					'sanitize_callback' => 'sanitize_text'
+			   ) );
+
+			$wp_customize->add_control( new WP_Customize_Control(
+				$wp_customize,
+				'header_quotation_text',
+					array(
+						'label'    => __( 'Quotation Text', 'adoptme' ),
+						'section'  => 'header_quotation_text',
+						'settings' => 'header_quot_block',
+						'type'     => 'text'
+					)
+				)
+			);
+
+			$wp_customize->add_section( 'header_author_text' , array(
+				'title'    => __('Change Author Name','adoptme'),
+				'panel'    => 'text_blocks_header',
+				'priority' => 10
+			) );
+
+				// Add setting
+				$wp_customize->add_setting( 'header_author_block', array(
+					'default'           => __( 'default text', 'adoptme' ),
+					'sanitize_callback' => 'sanitize_text'
+			   ) );
+
+			$wp_customize->add_control( new WP_Customize_Control(
+				$wp_customize,
+				'header_author_text',
+					array(
+						'label'    => __( 'Author Name', 'adoptme' ),
+						'section'  => 'header_author_text',
+						'settings' => 'header_author_block',
+						'type'     => 'text'
+					)
+				)
+			);
+			
+			 // Sanitize text
+			function sanitize_text( $text ) {
+				return sanitize_text_field( $text );
+			}
+		}
+
+		//dotad
+
+		//About text
+		add_action( 'customize_register', 'adoptme_about_text' );
+		/*
+		 * Register Our Customizer Stuff Here
+		 */
+		function adoptme_about_text( $wp_customize ) {
+			// Create custom panel.
+			$wp_customize->add_panel( 'text_blocks_about', array(
+				'priority'       => 500,
+				'theme_supports' => '',
+				'title'          => __( 'About text', 'adoptme' ),
+				'description'    => __( 'Changing about text at main site.', 'adoptme' ),
+			) );
+			// Add Footer Text
+			// Add section.
+			$wp_customize->add_section( 'custom_about_text' , array(
+				'title'    => __('Change About Text','adoptme'),
+				'panel'    => 'text_blocks_about',
+				'priority' => 10
+			) );
+		
+			// Add setting
+			$wp_customize->add_setting( 'about_block', array(
+				 'default'           => __( 'default text', 'adoptme' ),
+				 'sanitize_callback' => 'sanitize_about'
+			) );
+			// Add control
+			$wp_customize->add_control( new WP_Customize_Control(
+				$wp_customize,
+				'custom_header_text',
+					array(
+						'label'    => __( 'About text', 'adoptme' ),
+						'section'  => 'custom_about_text',
+						'settings' => 'about_block',
+						'type'     => 'text'
+					)
+				)
+			);
+			
+			 // Sanitize text
+			function sanitize_about( $text ) {
+				return sanitize_text_field( $text );
+			}
+		}
+
+		//About text końcówka
+		
+	}
+endif;
+add_action( 'after_setup_theme', 'adoptme_setup' );
+
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function adoptme_content_width() {
+	// This variable is intended to be overruled from themes.
+	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
+	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+	$GLOBALS['content_width'] = apply_filters( 'adoptme_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'adoptme_content_width', 0 );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function adoptme_widgets_init() {
+	register_sidebar( array(
+		'name'          => esc_html__( 'Sidebar', 'adoptme' ),
+		'id'            => 'sidebar-1',
+		'description'   => esc_html__( 'Add widgets here.', 'adoptme' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'adoptme_widgets_init' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function adoptme_scripts() {
+	wp_enqueue_style( 'adoptme-style', get_stylesheet_uri() );
+
+	wp_enqueue_script( 'adoptme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'adoptme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'adoptme_scripts' );
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+if ( defined( 'JETPACK__VERSION' ) ) {
+	require get_template_directory() . '/inc/jetpack.php';
+}
+
