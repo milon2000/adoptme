61e5f785e7646804c6e4fb049793f0b451ed2820
milon200 <milakozlowska2000@gmail.com>
2019-12-17 10:44:38 +0100

    featured cat section for mobile

--- front-page.php
+++ front-page.php
@@ -53,7 +53,7 @@ $my_query = new WP_Query($args);
         <div class="featured-cat__wrapper">
             <div class="featured-cat__cat-description">
             <h3 class="featured-cat__title"><?php the_title();?></h3>
-            <p class="adoptme__text featured-cat__description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+            <p class="adoptme__text featured-cat__description">eaque ipsa beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
             <a href="<?php the_permalink();?>"><button class="adoptme__button featured-cat__button">Adopt me</button></a>
             
         
@@ -61,8 +61,9 @@ $my_query = new WP_Query($args);
     <figure class="featured-cat__cat-photo">
             <img src="<?php the_post_thumbnail_url();?>"/>
         </figure><!--.featured-cat__cat-photo-->
-        </div><!--.featured-cat__wrapper--><?php
-        
+        </div><!--.featured-cat__wrapper-->
+        <?php
+      
   } wp_reset_query();?>
     </section>
 
